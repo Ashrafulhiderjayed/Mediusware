b470eb4f8483b4282a2972b8f4926b5b72037e43
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-01-15 19:55:43 +0600

    tast re-arrange according to instructions

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -58,18 +58,18 @@ const Problem1 = () => {
         <div className="col-8">
           <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
             <li className="nav-item">
-              <button className={`nav-link ${show === 'all' && 'active'}`} type="button" onClick={() => handleClick('all')}>
-                All
-              </button>
-            </li>
-            <li className="nav-item">
               <button className={`nav-link ${show === 'active' && 'active'}`} type="button" onClick={() => handleClick('active')}>
                 Active
               </button>
             </li>
             <li className="nav-item">
-              <button className={`nav-link ${show === 'completed' && 'active'}`} type="button" onClick={() => handleClick('completed')}>
-                Completed
+              <button className={`nav-link ${show === 'completed' && 'completed'}`} type="button" onClick={() => handleClick('completed')}>
+                completed
+              </button>
+            </li>
+            <li className="nav-item">
+              <button className={`nav-link ${show === 'all' && 'active'}`} type="button" onClick={() => handleClick('all')}>
+                All
               </button>
             </li>
           </ul>
